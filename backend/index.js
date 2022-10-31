dad4ae0834c9d118a1b1c5d18fbbf4e1c1515422
Mojtaba KAZMI <88171449+Mojtaba-Kazmi@users.noreply.github.com>
2022-10-31 12:38:45 +0100

    frontend files has been moved to frontend folder and backend folder has created and working is under progress

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import userRoutes from "./routes/users.js";
+const app = express();
+
+app.use("/api/users", userRoutes)
+app.listen(8080, () => {
+    console.log("API is working")
+});
